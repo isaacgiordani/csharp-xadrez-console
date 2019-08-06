f622cd7cc0341b6ee3c912acfa430222fb9f2bf3
Isaac Giordani <isaacgiordani@gmail.com>
2019-08-05 21:21:43 -0300

    Jogada especial promocao

--- xadrez-console/xadrez/PartidaDeXadrez.cs
+++ xadrez-console/xadrez/PartidaDeXadrez.cs
@@ -113,9 +113,23 @@ namespace xadrez {
 
         public void realizaJogada(Posicao origem, Posicao destino) {
             Peca pecaCapturada = executaMovimento(origem, destino);
+            
             if (estaEmXeque(jogadorAtual)) {
                 desfazMovimento(origem, destino, pecaCapturada);
                 throw new TabuleiroException("Você não pode se colocar em xeque!");
+            }
+            
+            Peca p = tab.peca(destino);
+
+            //#jogada especial promocao
+            if (p is Peao) {
+                if ((p.cor == Cor.Branca && destino.linha == 0) || (p.cor == Cor.Preta && destino.linha == 7)) {
+                    p = tab.retirarPeca(destino);
+                    pecas.Remove(p);
+                    Peca dama = new Dama(tab, p.cor);
+                    tab.colocarPeca(dama, destino);
+                    pecas.Add(dama); 
+                }
             }
 
             if (estaEmXeque(adversaria(jogadorAtual))) {
@@ -131,7 +145,7 @@ namespace xadrez {
                 mudaJogador();
             }
 
-            Peca p = tab.peca(destino);
+            
 
             //#jogada especial en passant
             if (p is Peao && (destino.linha == origem.linha - 2 || destino.linha == origem.linha + 2)) {
